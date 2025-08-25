cbcfa254dd810a76881092ba93cc08c85cc1cb84
mirhasankhan <mirhasan000034@gmail.com>
2025-08-25 10:55:50 +0600

    home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import Banner from "@/components/home/Banner";
-import Testimonials from "@/components/home/Testi";
 
 
 const HOmePage = () => {
   return (
     <div>
-      <Banner></Banner>
-      <Testimonials></Testimonials>
+      sdfsdfdf
  
     </div>
   );
