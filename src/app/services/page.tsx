16f158105861cf1075c17198313e7877aab17f57
mirhasankhan <mirhasan000034@gmail.com>
2025-08-29 09:23:52 +0600

    service card created

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { useServicesQuery } from "@/redux/features/services/services.api";
+import Container from "@/utils/Container";
+import Image from "next/image";
 
 const ServicePage = () => {
   const { data: legalServies } = useServicesQuery("");
-  console.log(legalServies?.data);
+
   return (
-    <div>
+    <Container>
       <div className="text-center">
         <h1 className="text-2xl font-medium text-primary">
           Our Practice Areas
@@ -14,9 +16,36 @@ const ServicePage = () => {
         <p>Expert legal representation across multiple practice areas</p>
         <div>
           {legalServies?.data?.length > 0 ? (
-            <div>
-              {legalServies?.data?.map((service) => (
-                <p key={service._id}>hello</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-12">
+              {legalServies?.data?.map((service: any) => (
+                <div className="border p-6" key={service._id}>
+                  <div className="flex flex-col pb-6 justify-center items-center">
+                    <div className="bg-primary p-2 rounded-xl">
+                      <Image
+                        className="h-12 w-12 rounded-[12px]"
+                        alt=""
+                        height={40}
+                        width={40}
+                        src={service.serviceIcon}
+                      ></Image>
+                    </div>
+                    <h1 className="text-xl font-medium py-2">{service.serviceName}</h1>
+                    <h1>
+                      {service.overview}sdf sdklflsdkjfl skjdflk
+                      sdjflksdjofisdjsdlk jslk;dfjl;k sjflks sdjflksdjflksd
+                    </h1>
+                  </div>
+                  <div>
+                    <h1 className="text-start">Key Features</h1>
+                    {service.features?.map((feature: string) => (
+                      <div className="flex gap-2 items-center" key={feature}>
+                        <h1 className="h-[8px] w-[8px] rounded-full bg-yellow-400"></h1>
+                        <h1 className="font-medium">{feature}</h1>
+                      </div>
+                    ))}
+                    <button className="bg-primary w-full text-white font-medium mt-auto py-2 rounded-[6px]">Find{service.serviceName} Lawyers</button>
+                  </div>
+                </div>
               ))}
             </div>
           ) : (
@@ -24,7 +53,7 @@ const ServicePage = () => {
           )}
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
