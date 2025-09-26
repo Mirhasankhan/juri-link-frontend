60c105e4fc8a78e58b124110843b205e9bdc821d
mirhasankhan <mirhasan000034@gmail.com>
2025-09-26 16:31:58 +0600

    post filter updated

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,80 +1,87 @@
-// "use client";
+"use client";
 
-// import Filters from "@/components/lawyers/Filters";
-// import PostCard from "@/components/posts/PostCard";
-// import SearchPosts from "@/components/posts/SearchPosts";
-// import Container from "@/utils/Container";
-// import React, { useState } from "react";
-// import { Funnel, X } from "lucide-react";
-// import { motion, AnimatePresence } from "framer-motion";
+import Filters from "@/components/lawyers/Filters";
+import PostCard from "@/components/posts/PostCard";
+import SearchPosts from "@/components/posts/SearchPosts";
+import Container from "@/utils/Container";
+import React, { useState } from "react";
+import { Funnel, X } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 
-// const PostsPage = () => {
-//   const [showMobileFilters, setShowMobileFilters] = useState(false);
+const PostsPage = () => {
+  const [showMobileFilters, setShowMobileFilters] = useState(false);
+  const [selectedYear, setSelectedYear] = useState<string | null>("");
+  const [selectedService, setSelectedService] = useState<string | null>("");
+  const [selectedLegal, setSelectedLegal] = useState<string | null>("");
 
-//   const toggleFilters = () => {
-//     setShowMobileFilters(!showMobileFilters);
-//   };
+  console.log(selectedYear,selectedService,selectedLegal);
 
-//   return (
-//     <div>
-//       <SearchPosts />
-//       <AnimatePresence>
-//         {showMobileFilters && (
-//           <motion.div
-//             initial={{ x: "-100%" }}
-//             animate={{ x: 0 }}
-//             exit={{ x: "-100%" }}
-//             transition={{ duration: 0.3 }}
-//             className="fixed top-0 left-0 w-3/4 h-full bg-white z-50 shadow-lg p-4 md:hidden"
-//           >
-//             <div className="flex justify-between items-center mb-4">
-//               <h2 className="text-lg font-semibold">Filters</h2>
-//               <button onClick={toggleFilters}>
-//                 <X size={24} />
-//               </button>
-//             </div>
-//             <Filters />
-//           </motion.div>
-//         )}
-//       </AnimatePresence>
+  const toggleFilters = () => {
+    setShowMobileFilters(!showMobileFilters);
+  };
 
-//       <Container>
-//         <div className="grid grid-cols-4 gap-5 mt-6">
-//           <div className="hidden md:block col-span-1">
-//             <Filters />
-//           </div>
-//           <div className="col-span-4 md:col-span-3">
-//             <div className="flex justify-between items-center pb-6">
-//               <h1 className="text-xl font-medium">
-//                 Found &quot;10 posts&quot;
-//               </h1>
-//               <button
-//                 onClick={toggleFilters}
-//                 className="md:hidden font-medium border px-4 py-1 rounded-[6px] flex gap-2 items-center"
-//               >
-//                 <Funnel size={18} />
-//                 Filters
-//               </button>
-//             </div>
-//             <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-//               <PostCard />
-//             </div>
-//           </div>
-//         </div>
-//       </Container>
-//     </div>
-//   );
-// };
-
-// export default PostsPage;
-import React from 'react';
-
-const page = () => {
   return (
     <div>
-      sdfsdfsdf
+      <SearchPosts />
+      <AnimatePresence>
+        {showMobileFilters && (
+          <motion.div
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%" }}
+            transition={{ duration: 0.3 }}
+            className="fixed top-0 left-0 w-3/4 h-full bg-white z-50 shadow-lg p-4 md:hidden"
+          >
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-lg font-semibold">Filters</h2>
+              <button onClick={toggleFilters}>
+                <X size={24} />
+              </button>
+            </div>
+            <Filters
+              selectedYear={selectedYear}
+              setSelectedYear={setSelectedYear}
+              selectedService={selectedService}
+              setSelectedService={setSelectedService}
+              selectedLegalService={selectedLegal}
+              setSelectedLegalService={setSelectedLegal}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <Container>
+        <div className="grid grid-cols-4 gap-5 mt-6">
+          <div className="hidden md:block col-span-1">
+            <Filters
+              selectedYear={selectedYear}
+              setSelectedYear={setSelectedYear}
+              selectedService={selectedService}
+              setSelectedService={setSelectedService}
+              selectedLegalService={selectedLegal}
+              setSelectedLegalService={setSelectedLegal}
+            />
+          </div>
+
+          <div className="col-span-4 md:col-span-3">
+            <div className="flex justify-between items-center pb-6">
+              <h1 className="text-xl font-medium">Found &quot;2 posts&quot;</h1>
+              <button
+                onClick={toggleFilters}
+                className="md:hidden font-medium border px-4 py-1 rounded-[6px] flex gap-2 items-center"
+              >
+                <Funnel size={18} />
+                Filters
+              </button>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+              <PostCard />
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
 
-export default page;
+export default PostsPage;
