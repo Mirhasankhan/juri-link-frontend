0a96099c3fa23fb6b17069523c01ea46f2c5a6d1
mirhasankhan <mirhasan000034@gmail.com>
2025-09-29 17:51:20 +0600

    verify updated

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -86,6 +86,10 @@ const VerifyEmail = () => {
     }
   };
 
+  if(!email){
+    router.push("/")
+  }
+
   return (
     <>
       <div className="min-h-screen bg-gradient-to-br from-purple-50 via-blue-50 to-indigo-100 flex items-center justify-center p-2">
