7fd5823fa8f0babfb16eb92d954b5f45b72486db
mirhasankhan <mirhasan000034@gmail.com>
2025-10-11 14:39:15 +0600

    profile image added

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -17,7 +17,7 @@ const PostCard = ({post}:{post:any}) => {
       <div className="flex gap-2">
         <img
           className="h-16 w-16 rounded-full"
-          src="https://images.unsplash.com/photo-1751716534754-e4eb69f18e90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
+          src={post?.userId?.profileImage}
         ></img>
         <div>
           <div>
