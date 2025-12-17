bf30c5ac0b98a63498c982afadd19517501429f1
mirhasankhan <mirhasan000034@gmail.com>
2025-12-17 17:03:31 +0600

    sdfsdf

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5006/api/v1/",
+    baseUrl: "https://api.passit.smtsigma.com/api/v1/",
     // baseUrl: "https://juri-link-server.vercel.app/api/v1/",
     prepareHeaders: (headers) => {
       const { token } = JWTDecode();
